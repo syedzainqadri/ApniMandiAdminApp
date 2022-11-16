d1f751c03a990189318df2e534e07df308131b54
EwigLife <zain@ewiglife.com>
2022-11-16 19:25:20 +0500

    admin updated

--- lib/views/login_screen.dart
+++ lib/views/login_screen.dart
@@ -1,6 +1,3 @@
-
-
-
 import 'package:apni_mandi_admin/controllers/login_controller.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -22,7 +19,6 @@ class LoginScreen extends StatelessWidget {
   double width = Get.width;
   final LoginController _loginController = Get.put(LoginController());
 
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -37,53 +33,52 @@ class LoginScreen extends StatelessWidget {
                 child: textStyle7(text: 'Welcome Back'),
               ),
               buildSpaceVertical(height * 0.02),
-              Center(child: Image.asset("assets/logo.png", height: height * 0.25, width: width * 0.5)),
+              Center(
+                  child: Image.asset("assets/logo.png",
+                      height: height * 0.25, width: width * 0.5)),
               buildSpaceVertical(height * 0.04),
-
               CustomTextField(
                 controller: emailController,
                 hintName: "ENTER EMAIL",
               ),
               buildSpaceVertical(height * 0.03),
-
               CustomTextField(
                 controller: passwordController,
                 hintName: "ENTER PASSWORD",
                 isPass: true,
               ),
-
               buildSpaceVertical(height * 0.06),
               InkWell(
                 onTap: () {
-                  if(_formKey.currentState!.validate()){
+                  if (_formKey.currentState!.validate()) {
                     _formKey.currentState!.save();
-                    if(emailController.text == "arrtiAdmin@gmail.com" && passwordController.text == "12345678"){
-                      _loginController.login(emailController.text.trim(), passwordController.text.trim());
-                    }else{
+                    if (emailController.text == "admin@apnimandi.com" &&
+                        passwordController.text == "12345678") {
+                      _loginController.login(emailController.text.trim(),
+                          passwordController.text.trim());
+                    } else {
                       errorToast("Error", "You are not Admin");
                     }
-
                   }
                 },
                 child: Obx(() {
-                  return  _loginController.isLoading.isTrue ?
-                  Center(
-                    child: Container(
-                        height: height * 0.06,
-                        width: width * 0.43,
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(AppSize.s26),
-                          color: ColorManager.primaryColor,
-                        ),
-                        child: const Center(child: CupertinoActivityIndicator())),
-                  )
-                      :  Center(child: actionButton('LOGIN', context));
+                  return _loginController.isLoading.isTrue
+                      ? Center(
+                          child: Container(
+                              height: height * 0.06,
+                              width: width * 0.43,
+                              decoration: BoxDecoration(
+                                borderRadius:
+                                    BorderRadius.circular(AppSize.s26),
+                                color: ColorManager.primaryColor,
+                              ),
+                              child: const Center(
+                                  child: CupertinoActivityIndicator())),
+                        )
+                      : Center(child: actionButton('LOGIN', context));
                 }),
               ),
-
               buildSpaceVertical(height * 0.1),
-
-
             ],
           ),
         ),
